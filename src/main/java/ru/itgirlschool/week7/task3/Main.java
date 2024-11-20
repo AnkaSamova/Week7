5740bcbb1950b520e697b6f91d83faceb123a08f
ANKA <annamaslovaspb@gmail.com>
2024-11-20 22:12:07 +0300

    first commit

--- src/main/java/ru/itgirlschool/week7/task3/Main.java
+++ src/main/java/ru/itgirlschool/week7/task3/Main.java
@@ -1,6 +1,7 @@
 package ru.itgirlschool.week7.task3;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
@@ -10,7 +11,7 @@ public class Main {
     }
 
     public static void characterCounter(String s) {
-        HashMap<Character, Integer> letters = new HashMap<>();
+        HashMap<Character, Integer> letters = new LinkedHashMap<>();
         for (char c : s.toCharArray()) {
             letters.compute(c, (k, count) -> (count == null) ? 1 : count + 1);
         }
